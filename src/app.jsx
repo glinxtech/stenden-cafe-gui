188c5e2d42daa4a26f1fbe681bbfaa514f041162
glinxtech <gooplsarrero@gmail.com>
2022-03-30 14:46:01 +0200

    Pages

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Layout from './components/layout';
+import Pages from './pages';
 
 function App() {
   return (
-    <Layout />
+    <Layout>
+      <Pages />
+    </Layout>
   );
 }
 
